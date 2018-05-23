a09b7b601e8fe5d19670770b0186df99e8bad2a3
YEKEYING <1935027415@qq.com>
2018-05-23 11:26:16 +0800

    5

--- battleship.js
+++ battleship.js
@@ -18,13 +18,13 @@ var view = {
 //view.displayHit("12");
 //view.displayMiss("25");
 //view.displayHit("26");
-//view.displayMessage("Tap tap, is this thing on?");
+view.displayMessage("Tap tap, is this thing on?");
+
 var model = {
   boardSize: 7,
   numShips : 3,
   shipLength: 3,
   shipsSunk: 0,
-
   ships: [ { locations: [0, 0, 0], hits: ["", "", ""] },
            { locations: [0, 0, 0], hits: ["", "", ""] },
            { locations: [0, 0, 0], hits: ["", "", ""] }],
@@ -60,7 +60,7 @@ var model = {
     for (var i = 0; i < this.numShips; i++) {
         do {
             locations = this.generateShip();
-        }while (this.conllision(locations));
+        } while (this.conllision(locations));
         this.ships[i].locations = locations;
     }
   },
@@ -70,7 +70,7 @@ var model = {
     if (direction === 1) {
         row = Math.floor(Math.random() * this.boardSize);
         col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
-    }else {
+    } else {
         row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
         col = Math.floor(Math.random() * this.boardSize);
     }
@@ -78,7 +78,7 @@ var model = {
     for (var i = 0; i < this.shipLength; i++) {
         if (direction === 1) {
           newShipLocations.push(row + "" + (col + i));
-        }else {
+        } else {
           newShipLocations.push((row + i) + "" + col);
         }
     }
@@ -109,28 +109,28 @@ var model = {
 
 function parseGuess(guess) {
   var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
-    if (guess === null || guess.length !==2) {
+  if (guess === null || guess.length !== 2) {
       alert("Oops, please enter a letter and a number on the board.");
-    }else {
+  } else {
       firstChar = guess.charAt(0);
       var row = alphabet.indexOf(firstChar);
       var column = guess.charAt(1);
       if (isNaN(row) || isNaN(column)) {
         alert("Oops, that isn't on the board.");
-      } else if (row < 0 || row >= model.boardSize ||
-                column < 0 || column >= model.boardSize) {
+      } else if (row < 0 || row >= model.boardSize || 
+                        column < 0 || column >= model.boardSize) {
           alert("Oops, that's off the board!");
       } else {
-        return row + column;
+          return row + column;
       }
-    }
-    return null;
+  }
+  return null;
 }
-//console.log(parseGuess("A0"));
-//console.log(parseGuess("B6"));
-//console.log(parseGuess("G3"));
-//console.log(parseGuess("H0"));
-//console.log(parseGuess("A7"));
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
 var controller = {
   guesses: 0,
   processGuess: function(guess) {
@@ -139,8 +139,7 @@ var controller = {
       this.guesses++;
       var hit = model.fire(location);
       if (hit && model.shipsSunk === model.numShips) {
-        view.displayMessage("You sank all my battleships, in " +
-                                          this.guesses + " guesses");
+        view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
       }
     }
   }
@@ -155,18 +154,18 @@ var controller = {
 //controller.processGuess("B0");
 //controller.processGuess("B1");
 //controller.processGuess("B2");
-function handleFireButton() {
-  var guessInput = document.getElementById("guessInput");
-  var guess = guessInput.value;
-  controller.parseGuess(guess);
-  guessInput.value = "";
-}
 function init() {
   var fireButton = document.getElementById("fireButton");
   fireButton.onclick = handleFireButton;
   var guessInput = document.getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
   model.generateShipLocations();
+}
+function handleFireButton() {
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = "";
 }
 window.onload = init;
 function handleKeyPress(e) {
